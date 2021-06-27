c8f9f79d06ed25b90ad98720407a8ee637683d95
felix <felixprograms1205@gmail.com>
2021-06-27 09:32:41 +0800

    27/6

--- iteration/exercises/spec/all_pattern_spec.rb
+++ iteration/exercises/spec/all_pattern_spec.rb
@@ -24,16 +24,21 @@ RSpec.describe 'all pattern' do
     expect(all_zeros).to be true
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     words = ["love", "hate", "fire", "bird", "call"]
     all_four_letters = true
     words.each do |word|
       # Your Code Here
+      if word.length == 4
+        all_four_letters = true
+      else
+        all_four_letters = false
+      end
     end
     expect(all_four_letters).to be true
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     words = {
       one: "love",
       two: "hate",
@@ -44,19 +49,26 @@ RSpec.describe 'all pattern' do
     all_four_letters = true
     words.each do |position, word|
       # Your Code Here
+      unless word.length == 4
+        all_four_letters = false
+      end
     end
     expect(all_four_letters).to be true
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     statuses = [:busy, :busy, :busy]
     all_busy = true
     # Your Code Here
-
+    statuses.each do |status|
+      unless status == :busy
+        all_busy = false
+      end
+    end
     expect(all_busy).to be true
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     friend_status = {
       "Megan" => :busy,
       "Sarah" => :busy,
@@ -64,18 +76,24 @@ RSpec.describe 'all pattern' do
     }
     all_busy = true
     # Your Code Here
-
+    friend_status.each do |friend, state|
+      all_busy = false if state != :busy
+    end
     expect(all_busy).to be true
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     zip_codes = [94381, 831, 50009, 36232, 8992, 89999, 11110]
+    all_five_digits = true
     # Your Code Here
+    zip_codes.each do |zipcode|
+      all_five_digits = false unless zipcode < 100000 && zipcode > 9999
+    end      
 
     expect(all_five_digits).to be false
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     zip_codes = {
       "Megan" => 94381,
       "Sarah" => 831,
@@ -85,26 +103,42 @@ RSpec.describe 'all pattern' do
       "Cameron" => 89999,
       "Joshua" => 11110
     }
+    all_five_digits = true
     # Your Code Here
+    zip_codes.each do |name, code|
+      all_five_digits = false unless code < 100000 && code > 9999
+    end
+      
 
     expect(all_five_digits).to be false
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     snacks = ["GARLIC PLANTAINS", "SNICKERDOODLES", "Pretzels"]
     # Your Code Here
+    all_caps = true
+    snacks.each do |s|
+      if s != s.upcase
+        all_caps = false
+      end
+    end
 
-    assert_equal false, all_caps
     expect(all_caps).to be false
   end
 
-  xit 'test 10' do
+  it 'test 10' do
     snacks = {
       savory: "GARLIC PLANTAINS",
       sweet: "SNICKERDOODLES",
       salty: "Pretzels"
     }
     # Your Code Here
+    all_caps = true
+    snacks.each do |a, b|
+      if b != b.upcase
+        all_caps = false
+      end
+    end
 
     expect(all_caps).to be false
   end
